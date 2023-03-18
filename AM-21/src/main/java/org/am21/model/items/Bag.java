a70df1e2e4ce7b1c213f242876e9ea34424f95d6
Leo <74366736+Leomyh@users.noreply.github.com>
2023-03-18 19:30:22 +0100

    first code by Bag and Shelf

--- AM-21/src/main/java/org/am21/model/items/Bag.java
+++ AM-21/src/main/java/org/am21/model/items/Bag.java
@@ -1,4 +1,21 @@
 package org.am21.model.items;
 
 public class Bag {
+    private int itemNum; //initial is 132 item
+    private Collection<ItemTileCard> itemCollection;
+
+    public Bag(){
+        this.itemNum = 132;
+        this.itemCollection = new ItemTileCard();
+    }
+
+    public void addItem(){
+        //once a game (132 item)
+        this.itemNum=132;
+    }
+
+    public void drawItem(int requestNum){
+        this.itemNum -=requestNum;
+        //pick the the different card by ItemTileCard
+    }
 }
--- AM-21/src/main/java/org/am21/model/items/Shelf.java
+++ AM-21/src/main/java/org/am21/model/items/Shelf.java
@@ -1,4 +1,36 @@
 package org.am21.model.items;
 
 public class Shelf {
+    private int rowNum = 6;
+    private int colNum = 5;
+    private Cell[][] cells;
+    private int maxCard;
+    private Player shelfId;
+
+    public List<List<ItemTiteCard>> bookshelfGrid;
+
+    public Shelf(){
+        this.maxCard=rowNum*colNum;
+        this.cells= new Cell[row][col];
+        this.bookshelfGrid=new Shelf();
+        this.shelfId= Player(); //associate shelf of player
+    }
+
+    public Shelf myShelf(){
+        //my shelf item matrix
+        Shelf myPersonalShelf= new Shelf();
+        Cell[][] cells = Shelf.getCells();
+    }
+    public void getCard(){
+        //get card from player
+    }
+
+
+    public void setCard(){
+        //put card into shelf
+        //select the the colum from 1 to 3
+    }
+
+
+
 }
