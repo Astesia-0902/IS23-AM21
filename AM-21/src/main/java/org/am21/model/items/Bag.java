fa8841e8c06c3014cf5994adb413dd41d8914e11
Leo <74366736+Leomyh@users.noreply.github.com>
2023-03-19 18:13:17 +0100

    change something

--- AM-21/src/main/java/org/am21/model/items/Bag.java
+++ AM-21/src/main/java/org/am21/model/items/Bag.java
@@ -5,19 +5,19 @@ import org.am21.model.items.Card.ItemTileCard;
 public class Bag {
     private int itemNum; //initial is 132 item
     //private Collection<ItemTileCard> itemCollection;
-
+    public int requestNum;
     public Bag(){
         this.itemNum = 132;
         //this.itemCollection = new ItemTileCard();
     }
 
-    public void addItem(){
-        //once a game (132 item)
-        this.itemNum=132;
+    public int getRequestNum(){
+        return requestNum;
     }
 
-    public void drawItem(int requestNum){
-        this.itemNum -=requestNum;
-        //pick the the different card by ItemTileCard
+    public void drawItem(){
+        this.itemNum -= requestNum;
+        //pick the different card by ItemTileCard
     }
 }
+
--- AM-21/src/main/java/org/am21/model/items/Shelf.java
+++ AM-21/src/main/java/org/am21/model/items/Shelf.java
@@ -5,38 +5,60 @@ import org.am21.model.items.Card.ItemTileCard;
 
 import java.util.ArrayList;
 
-public class Shelf extends Grid {
-//    private int rowNum = 6;
-//    private int colNum = 5;
-//    private Cell[][] cells;
-//    private int maxCard;
+public class Shelf {
+    private int rowNum = 6;
+    private int colNum = 5;
+    private Cell[][] cells;
+    private int maxCardAvailable;
+
+    //state of col available (default true)
+    private boolean col1;
+    private boolean col2;
+    private boolean col3;
+    private boolean col4;
+    private boolean col5;
     public Player player;
+
     public ArrayList<ArrayList<ItemTileCard>> bookshelfGrid;
 
-    public Shelf() {
-        super(6, 5);
+    public Shelf(){
+        this.maxCardAvailable=rowNum*colNum;
+        this.col1 = true;
+        this.col2 = true;
+        this.col3 = true;
+        this.col4 = true;
+        this.col5 = true;
+        this.cells= new Cell[rowNum][colNum];
         this.bookshelfGrid=new ArrayList<ArrayList<ItemTileCard>>();
     }
 
     public Shelf myShelf(){
         //my shelf item matrix
         Shelf myPersonalShelf= new Shelf();
-        //Cell[][] cells = Shelf.Cells();
         return null;
     }
 
-    public void getCard(){
-        //get card from player
+    public int getColNum(){
+        return colNum;
     }
 
 
-    public void setCard(){
-        //put card into shelf
-        //select the  colum from 1 to 3
+    private static Cell[][] getCells() {
+
+        return null;
+    }
+
+    public void setCard(ItemTileCard n1, ItemTileCard n2, ItemTileCard n3){
+        //selected the n° column to fill card
+        //put card into shelf from playerhand
+
     }
 
 
     public int slotAvailable(int colNum) {
-        return 0;
+
+        this.colNum-=colNum;
+        return this.colNum;
     }
 }
+
