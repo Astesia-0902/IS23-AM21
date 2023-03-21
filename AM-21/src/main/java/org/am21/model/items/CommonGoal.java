8d99ac9a89f722079a2c24c7be4a095767d5c7d8
KenChen <ken.chen@mail.polimi.it>
2023-03-21 16:24:50 +0100

    Correction CommonGoal abstract class

--- AM-21/src/main/java/org/am21/model/items/CommonGoal.java
+++ AM-21/src/main/java/org/am21/model/items/CommonGoal.java
@@ -1,18 +1,28 @@
 package org.am21.model.items;
 
+import org.am21.model.Player;
 import org.am21.model.items.Card.ScoringTokenCard;
 
-public class CommonGoal extends Goal {
-    private ScoringTokenCard scoringTokenCard;
+import javax.swing.text.PlainDocument;
+import java.util.List;
 
-    public ScoringTokenCard getScoringTokenCard() {
-        return scoringTokenCard;
-    }
+public abstract class CommonGoal extends Goal {
+    private int id;
+    public List<ScoringTokenCard> tokenStack;
 
-    public void setScoringTokenCard(ScoringTokenCard scoringTokenCard) {
-        this.scoringTokenCard = scoringTokenCard;
-    }
+    public List<Player> achievedPlayers;
 
+
+
+    public CommonGoal(){};
+
+    /**
+     * Durante la fase del turno CheckingGoal, viene controllato la Shelf del Player
+     * per verificare se l'obiettivo è stato completato
+     *
+     * @param shelf
+     * @return
+     */
     public boolean checkGoal(Shelf shelf) {
         return false;
     }
