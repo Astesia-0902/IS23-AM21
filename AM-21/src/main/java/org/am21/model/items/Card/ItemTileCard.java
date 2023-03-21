12f9a59860e7f614f50d965783c5a0c5ee53091c
SimonaCai <simona.cai@mail.polimi.it>
2023-03-21 17:02:54 +0100

    Update ItemTileCard.java

--- AM-21/src/main/java/org/am21/model/items/Card/ItemTileCard.java
+++ AM-21/src/main/java/org/am21/model/items/Card/ItemTileCard.java
@@ -4,9 +4,9 @@ import java.util.*;
 public class ItemTileCard extends Card {
     private boolean isNonSelectable;
 
-    private final static List<String> itemTileCards = new ArrayList<>();
+    private final static List<String> itemTileNames = new ArrayList<>();
     static {
-        Collections.addAll(itemTileCards,ItemType.Cats.name()+"1.1",ItemType.Cats.name()+"1.2",ItemType.Cats.name()+"1.3",
+        Collections.addAll(itemTileNames,ItemType.Cats.name()+"1.1",ItemType.Cats.name()+"1.2",ItemType.Cats.name()+"1.3",
                 ItemType.Books.name()+"1.1",ItemType.Books.name()+"1.2",ItemType.Books.name()+"1.3",
                 ItemType.Games.name()+"1.1",ItemType.Games.name()+"1.2",ItemType.Games.name()+"1.3",
                 ItemType.Frames.name()+"1.1",ItemType.Frames.name()+"1.2",ItemType.Frames.name()+"1.3",
@@ -19,7 +19,14 @@ public class ItemTileCard extends Card {
         this.isNonSelectable = false;
     }
 
-    public static List<String> buildItemTileCards(){
+    public static List<ItemTileCard> buildItemTileCards(){
+        List<ItemTileCard> itemTileCards = new ArrayList<>();
+        for (int i = 0; i < itemTileNames.size(); i++) {
+            for (int j = 0; j < 22; j++) {
+                ItemTileCard itemTileCard = new ItemTileCard(itemTileNames.get(i));
+                itemTileCards.add(itemTileCard);
+            }
+        }
         Collections.shuffle(itemTileCards);
         return itemTileCards;
     }
