9692a056f99c554fa8531c445b8566cfe467a2d0
Leo <74366736+Leomyh@users.noreply.github.com>
2023-04-02 12:27:28 +0200

    add method Shelf as request

--- AM-21/src/main/java/org/am21/model/items/Shelf.java
+++ AM-21/src/main/java/org/am21/model/items/Shelf.java
@@ -1,6 +1,7 @@
 package org.am21.model.items;
 
 import org.am21.model.Player;
+import org.am21.model.items.Card.ItemTileCard;
 import org.am21.utilities.Coordinates;
 
 import java.util.ArrayList;
@@ -8,64 +9,115 @@ import java.util.List;
 
 /**
  * Attributes needed:
- * - Number of cells available for each column
- * - Total number of cells available in the Shelf
- * - Limit for insertion overall (it could be 3,2 or 1)
+ * - Number of cells available for each column              <--check by method getSlotCol1,2,3,4,5
+ * - Total number of cells available in the Shelf           <--check by method slotTotAvailable
+ * - Limit for insertion overall (it could be 3,2 or 1)  <--check by method slotTotAvailable
  *
  * Method needed:
- * - Insertion of one item in a column(cells available in the column will decrease)
+ * - Insertion of one item in a column(cells available in the column will decrease)  <--check by method pushCard
  * -
  */
 
 public class Shelf extends Grid {
     /**state of col available (default true)*/
-    public boolean col1;
-    public boolean col2;
-    public boolean col3;
-    public boolean col4;
-    public boolean col5;
+
+    public int slotCol1;
+    public int slotCol2;
+    public int slotCol3;
+    public int slotCol4;
+    public int slotCol5;
 
     public Player player;
 
 
     public Shelf(){
         super(6, 5);
-        this.col1 = true;
-        this.col2 = true;
-        this.col3 = true;
-        this.col4 = true;
-        this.col5 = true;
-
-    }
-
-    /**selected the number of column to fill card*/
-    public void pushCard(Cell item, int colNum){
-
-        //this.bookshelfGrid.get(colNum).add(n);
-        /**rolling the first row available*/
-        for(int i=0;i<6;i++){
-            /**if cell available fill in*/
-            if (this.getItemName(i,colNum)==null) {
-                this.setCell(i,colNum,item);
-            }
-        }
+        this.slotCol1 = 6;
+        this.slotCol2 = 6;
+        this.slotCol3 = 6;
+        this.slotCol4 = 6;
+        this.slotCol5 = 6;
 
     }
 
     /**number of slot available*/
-    public int slotAvailable() {
-        int colNum=0;
-        if(col1)
-            colNum++;
-        if(col2)
-            colNum++;
-        if(col3)
-            colNum++;
-        if(col4)
-            colNum++;
-        if(col5)
-            colNum++;
-        return colNum;
+    public int slotTotAvailable() {
+        return this.slotCol1+this.slotCol2+this.slotCol3+this.slotCol4+this.slotCol5;
+    }
+    public int getCol1(){return this.slotCol1;}
+    public int getCol2(){return this.slotCol2;}
+    public int getCol3(){return this.slotCol3;}
+    public int getCol4(){return this.slotCol4;}
+    public int getCol5(){return this.slotCol5;}
+    private void setCol1(int numDec ){ this.slotCol1-=numDec;}
+    private void setCol2(int numDec ){ this.slotCol2-=numDec;}
+    private void setCol3(int numDec ){ this.slotCol3-=numDec;}
+    private void setCol4(int numDec ){ this.slotCol4-=numDec;}
+    private void setCol5(int numDec ){ this.slotCol5-=numDec;}
+
+    public void insertCard(ItemTileCard item, int numCard,int numCol){
+        /** fill the first cell available*/
+        switch (numCol) {
+            case 1: if(numCard<=getCol1())
+                    {
+                        for (int i = 6 - getCol1(); numCard != 0; i++)
+                        {
+                            this.insertInCell(i, 1, item);
+                            numCard--;
+                        }
+                        setCol1(numCard);
+                    }
+                    /** else return false */
+                    break;
+            case 2: if(numCard<=getCol2())
+                    {
+                        for (int i = 6 - getCol2(); numCard != 0; i++)
+                        {
+                            this.insertInCell(i, 2, item);
+                            numCard--;
+                        }
+
+                        setCol2(numCard);
+                    }
+                /** else return false */
+                break;
+            case 3: if(numCard<=getCol3())
+                    {
+                        for (int i = 6 - getCol3(); numCard != 0; i++)
+                        {
+                            this.insertInCell(i, 3, item);
+                            numCard--;
+                        }
+
+                        setCol3(numCard);
+                    }
+                /** else return false */
+                break;
+            case 4: if(numCard<=getCol4())
+                    {
+                        for (int i = 6 - getCol4(); numCard != 0; i++)
+                        {
+                            this.insertInCell(i, 4, item);
+                            numCard--;
+                        }
+
+                        setCol4(numCard);
+                    }
+                /** else return false */
+                break;
+            case 5: if(numCard<=getCol5())
+                    {
+                        for (int i = 6 - getCol5(); numCard != 0; i++)
+                        {
+                            this.insertInCell(i, 5, item);
+                            numCard--;
+                        }
+
+                        setCol5(numCard);
+                    }
+                /** else return false */
+                break;
+        }
     }
 
     /**adjacent same item check */
@@ -86,16 +138,16 @@ public class Shelf extends Grid {
                         /** save the first not null item in String type*/
                         String recentCard = this.getItemName(common.get(reg_index).x,common.get(reg_index).y);
 
-                        if (this.getItemName(r, c + 1) != null && (recentCard == this.getItemName(r, c + 1))) {
+                        if (this.getItemName(r, c + 1) != null && (recentCard.equals(this.getItemName(r, c + 1)) )) {
                             common.add(new Coordinates(r, c + 1));
                         }
-                        if (this.getItemName(r, c - 1) != null && (recentCard == this.getItemName(r, c - 1))) {
+                        if (this.getItemName(r, c - 1) != null && (recentCard.equals(this.getItemName(r, c - 1)) )) {
                             common.add(new Coordinates(r, c - 1));
                         }
-                        if (this.getItemName(r + 1, c) != null && (recentCard == this.getItemName(r + 1, c))) {
+                        if (this.getItemName(r + 1, c) != null && (recentCard.equals(this.getItemName(r + 1, c)) )) {
                             common.add(new Coordinates(r + 1, c));
                         }
-                        if (this.getItemName(r - 1, c) != null && (recentCard == this.getItemName(r - 1, c))) {
+                        if (this.getItemName(r - 1, c) != null && (recentCard.equals(this.getItemName(r - 1, c)) )) {
                             common.add(new Coordinates(r - 1, c));
                         }
                         /** set the cell to null*/
