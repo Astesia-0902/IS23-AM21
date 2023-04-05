d3b235c75f0db25f8fe01be65b20f3e7efe1a728
SimonaCai <simona.cai@mail.polimi.it>
2023-04-05 10:43:19 +0200

    delete ScoringTokenCard.java

--- AM-21/src/main/java/org/am21/model/Card/CommonGoal.java
+++ AM-21/src/main/java/org/am21/model/Card/CommonGoal.java
@@ -5,28 +5,16 @@ import org.am21.model.Shelf;
 import org.am21.utilities.CardUtil;
 
 import java.util.List;
-import java.util.Stack;
 
 public abstract class CommonGoal extends Goal {
     public List<Integer> tokenStack;
     public List<Player> achievedPlayers;
     public int index;
-//    public Stack<Integer> scoreBox ;
-
-    /*
-    public CommonGoal(int id, List<Player> achievedPlayers) {
-        super();
-        this.id = id;
-        this.achievedPlayers = achievedPlayers;
-        this.tokenStack = new ArrayList<>();
-    }*/
 
     public CommonGoal(String nameCard,int numPlayer){
         super(nameCard);
         this.tokenStack = CardUtil.buildScoringTokenCards(numPlayer);
         this.index = 0;
-//        this.scoreBox = new Stack<>();
-//        pushScores(numPlayer);
     }
 
     public boolean checkGoal(Shelf shelf) {
@@ -48,38 +36,6 @@ public abstract class CommonGoal extends Goal {
 
         index++;
     }
-
-//    public boolean pushScores(int numPlayer)   {
-//        switch(numPlayer) {
-//            case 2:
-//                this.scoreBox.push(4);
-//                this.scoreBox.push(8);
-//                break;
-//            case 3:
-//                this.scoreBox.push(4);
-//                this.scoreBox.push(6);
-//                this.scoreBox.push(8);
-//                break;
-//            case 4:
-//                this.scoreBox.push(2);
-//                this.scoreBox.push(4);
-//                this.scoreBox.push(6);
-//                this.scoreBox.push(8);
-//                break;
-//            default:
-//                return false;
-//        }
-//        return true;
-//    }
-//
-//    public boolean popScores(Stack<Integer> scoreBox) {
-//        if (scoreBox.empty())
-//            return false;
-//        else {
-//            scoreBox.pop();
-//            return true;
-//        }
-//    }
 }
 
 
--- AM-21/src/main/java/org/am21/model/Card/Goal.java
+++ AM-21/src/main/java/org/am21/model/Card/Goal.java
@@ -1,9 +1,7 @@
 package org.am21.model.Card;
 
 public abstract class Goal extends Card {
-    public int goalID;
     public int score;
-
     public Goal(String nameCard) {
         super(nameCard);
     }
