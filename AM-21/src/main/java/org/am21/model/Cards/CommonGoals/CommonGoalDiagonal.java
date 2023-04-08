967690b23afb83f0326108acf4269c62e1409764
KenChen <ken.chen@mail.polimi.it>
2023-04-08 18:11:30 +0200

    Update CommongoalDiagonal

--- AM-21/src/main/java/org/am21/model/Cards/CommonGoals/CommonGoalDiagonal.java
+++ AM-21/src/main/java/org/am21/model/Cards/CommonGoals/CommonGoalDiagonal.java
@@ -4,7 +4,7 @@ import org.am21.model.Cards.CommonGoal;
 import org.am21.model.items.Shelf;
 
 public class CommonGoalDiagonal extends CommonGoal {
-    private static final int nDiagPerSide = 2;
+    private static final int nDiag = 2;
 
     /**
      *
@@ -28,52 +28,21 @@ public class CommonGoalDiagonal extends CommonGoal {
         //  (0,0), (1,1), (2,2), (3,3), (4,4)
         //  (1,0), (2,1), (3,2), (4,3), (5,4)
 
-        //  (0,4), (1,3), (2,2), (3,1), (4,0)
-        //  (1,4), (2,3), (3,2), (4,1), (5,0)
+
         // Diagonals that start from bottom left
-        for(int d = 0; d< nDiagPerSide;d++){
+        for(int r = 0; r < nDiag; r++){
             state = false;
-            if(s.isOccupied(d,0)) {
-                String name = s.getItemName(d, 0).substring(0, s.getItemName(d, 0).length() - 3);
+            if(s.isOccupied(r,0)) {
+                String type = s.getItemType(r, 0);
                 // Scan one diagonal
-                for (int r = d+1, c = 1; r < (Shelf.sRow - 1 + d) && c < Shelf.sColumn; r++, c++) {
-                    if(s.isOccupied(r,c) &&
-                            name.equals(s.getItemName(r,c)
-                                    .substring(0,s.getItemName(r,c).length()-3)))
-                    {
-                        state = true;
-                    }else{
-                        state = false;
-                        break;
-                    }
-                }
-                if(state){
+                if(s.isOccupied(r,0)&& type.equals(s.getItemType(r,0))&&
+                s.isOccupied(r+1,1)&& type.equals(s.getItemType(r+1,1))&&
+                s.isOccupied(r+2,2)&&type.equals(s.getItemType(r+2,2))&&
+                s.isOccupied(r+3,3)&&type.equals(s.getItemType(r+3,3))&&
+                s.isOccupied(r+4,4)&&type.equals(s.getItemType(r+4,4))){
                     return true;
                 }
-            }
 
-        }
-
-        // Diagonals that start from bottom right
-        for(int p = 0; p < nDiagPerSide; p++){
-            state = false;
-            if(s.isOccupied(p,4)) {
-                String name = s.getItemName(p, 4).substring(0, s.getItemName(p, 4).length() - 3);
-                // Scan one diagonal
-                for (int r = p +1, c = 3; r < (Shelf.sRow - 1 + p) && c>=0; r++, c--) {
-                    if(s.isOccupied(r,c) &&
-                            name.equals(s.getItemName(r,c)
-                                    .substring(0,s.getItemName(r,c).length()-3)))
-                    {
-                        state = true;
-                    }else{
-                        state = false;
-                        break;
-                    }
-                }
-                if(state){
-                    return true;
-                }
             }
 
         }
@@ -82,7 +51,9 @@ public class CommonGoalDiagonal extends CommonGoal {
 
 
 
-        return state;
+
+
+        return false;
     }
 }
 
@@ -98,3 +69,33 @@ public class CommonGoalDiagonal extends CommonGoal {
                     substring(0, shelf.getItemName(row+4, 4).length() - 3))){
                     return true;
                     }*/
+
+
+
+//  (0,4), (1,3), (2,2), (3,1), (4,0)
+//  (1,4), (2,3), (3,2), (4,1), (5,0)
+
+/*// Diagonals that start from bottom right
+        for(int p = 0; p < nDiagPerSide; p++){
+        state = false;
+        if(s.isOccupied(p,4)) {
+        String name = s.getItemName(p, 4).substring(0, s.getItemName(p, 4).length() - 3);
+        // Scan one diagonal
+        for (int r = p +1, c = 3; r < (Shelf.sRow - 1 + p) && c>=0; r++, c--) {
+        if(s.isOccupied(r,c) &&
+        name.equals(s.getItemName(r,c)
+        .substring(0,s.getItemName(r,c).length()-3)))
+        {
+        state = true;
+        }else{
+        state = false;
+        break;
+        }
+        }
+        if(state){
+        return true;
+        }
+        }
+        }
+
+    */
